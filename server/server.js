c3de1b511f9203f028ce568dd0ca58cd5ea6c3d9
Andrii Labyk <andrii.labyk@gmail.com>
2018-05-28 14:30:41 +0300

    add structure

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
+const http = require('http');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
 
 const config = require('../config');
 const port = config.port;
 
 const app = express();
 const router = express.Router();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 
-const server = app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use(express.static(publicPath));
 
 app.use('/', router);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
 
 console.log(publicPath);
